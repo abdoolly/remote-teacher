a3328554bbea8e28775947eb1aab6a7ba66241f3
abdoolly <abdallahgamal@Abdallahs-MacBook-Pro.local>
2020-03-27 19:11:03 +0200

    added the typeDef

--- src/app.ts
+++ src/app.ts
@@ -15,27 +15,86 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// basic typeDef
 const typeDefs = gql`
 enum UserType {
     TEACHER
     STUDENT
 }
 
+type Subject {
+    name: String!
+    photoUrl: String
+    grade: String
+}
+
 type User {
+    _id: ID!
     fullName: String!
     phone: String!
     userType: UserType!
+    password: String
+    grade: String
+    subjects: [Subject]
+}
+
+input UserInput {
+    fullName: String!
+    phone: String!
     password: String!
+    confirmPassword: String!
 }
 
 type Query {
-    me: User
+    login(phone: String!, password: String): User!
+    getTeachers(firstTen: Boolean,grade: String, name: String): [User]!
+    getTeacher(id: ID!) : User
+    getSubjects(grade: String) : [Subject]!
+}
+
+type RegisterOutput {
+    user: User!
+    token : String!
+}
+
+type Mutation {
+    registerTeacher(data: UserInput!) : RegisterOutput!
+    registerStudent(data: UserInput!) : RegisterOutput!
 }
 `;
 
+const grades = [
+    'Primary 1',
+    'Primary 2',
+    'Primary 3',
+    'Primary 4',
+    'Primary 5',
+    'Primary 6',
+    '1st prep',
+    '2nd prep',
+    '3rd prep',
+    '1st secondry',
+    '2nd secondry',
+    '3rd secondry'
+];
+
+// mocking layer
 const mocks = {
     User: () => ({
-        type: casual.random_element(['STUDENT', 'TEACHER'])
+        _id: casual._uuid(),
+        fullName: casual._first_name() + ' ' + casual._last_name(),
+        phone: casual._phone,
+        userType: casual.random_element(['STUDENT', 'TEACHER']),
+        password: casual._password,
+        grade: casual.random_element(grades),
+    }),
+    RegisterOutput: () => ({
+        token: casual._uuid(),
+    }),
+    Subject: () => ({
+        name: casual.random_element(['English', 'Arabic', 'Math', 'Science', 'Psychology']),
+        photoUrl: casual._url(),
+        grade: casual.random_element(grades)
     })
 };
 
